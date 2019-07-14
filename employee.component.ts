2d7fd0c7f2a6dbe1598c909f94a80a49d96a2d27
manoj duli <manojduli123@gmail.com>
2019-07-15 00:42:10 +0530

    Update employee.component.ts

--- employee.component.ts
+++ employee.component.ts
@@ -1,78 +1,76 @@
-import { Component, OnInit } from '@angular/core';
-import { Alert } from 'selenium-webdriver';
-import { hasLifecycleHook } from '@angular/compiler/src/lifecycle_reflector';
-
+import { Component } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Empinterface } from './empinterface';
 
 @Component({
-  selector: 'app-employee',
-  templateUrl: './employee.component.html',
-  styleUrls: ['./employee.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class EmployeeComponent implements OnInit {
+export class AppComponent {
+  title = 'crud';
+  employee: Empinterface[]; /* =
+  [
+    {
+      empId: 1001,
+      empName: 'Rahul',
+      empSal: 9000,
+      empDep: 'Java'
+    },
+    {
+      empId: 1002,
+      empName: 'Sachin',
+      empSal: 19000,
+      empDep: 'OraApps'
+    },
+    {
+      empId: 1003,
+      empName: 'Vikash',
+      empSal: 29000,
+      empDep: 'BI'
+    }
+  ];*/
+
+b1: number;
+b2: string;
+b3: number;
+b4: any;
+  d: any;
   a: any;
   b: any;
   c: any;
-  d: any;
   res: any;
+addemp(a1: number, a2: string, a3: number, a4: string) {
+  this.b1 = a1;
+  this.b2 = a2;
+  this.b3 = a3;
+  this.b4 = a4;
+  this.employee.push({"empId": this.b1,"empName": this.b2,"empSal": this.b3,"empDep": this.b4});
+  /*this.employee.push({'empId': a1,'empName': a2,'empSal': a3,'empDep': a4});*/
+}
+delete(id: number) {
+  this.employee.splice(id, 1);
+}
+updateemp(emp) {
+  this.a = emp.empId;
+  this.b = emp.empName;
+  this.c = emp.empSal;
+  this.d = emp.empDep;
+  this.res = emp;
+}
+update(c1: number, c2: string, c3: number , c4: string) {
+this.res.empId = c1;
+this.res.empName = c2;
+this.res.empSal = c3;
+this.res.empDep = c4;
+}
 
-  constructor() { }
-  employee=
-  
-    [
-      { 
-        empId:1001,
-        empName:"Rahul",
-        empSal:9000,
-        empDep:"Java"
-        
-      },
-      {
-        empId:1002
-        ,empName:"Sachin",
-        empSal:19000,
-        empDep:"OraApps"
-      },
-      {empId:1003,
-        empName:"Vikash",
-        empSal:29000,
-        empDep:"BI"
-      },
-      ];
-
-  
-  ngOnInit() {
-  }
-
-  addemployee(id:number,name:string,sal:number,dep:any){
-   let t1=id;
-   let t2=name;
-   let t3=sal;
-   let t4=dep;
-   this.employee.push({"empId":t1,"empName":t2,"empSal":t3,"empDep":t4});
-  }
-  Delete(empId:number)
-  {
-      alert("Do you want to delete");
-      this.employee.splice(empId,1)
-  }
-  update(emp)
-  {
-      this.a=emp.empId;
-      this.b=emp.empName;
-      this.c=emp.empSal;
-      this.d=emp.empDep;
-      this.res=emp;
-  }
-  update1(id1:number,name1:string,sal1:string,dep1:number)
-  {
-    alert("update empId,empName,empSal,empDep for"  +id1);
-    this.res.empId=id1;
-    this.res.empName=name1;
-    this.res.empSal=sal1;
-    this.res.empDep=dep1;
-
-
-  }
-
-
+/*constructor(private httpService: HttpClient) {
+  this.httpService.get('assets/empdata.json').subscribe(
+     data => { this.employee = data as Empinterface[];
+  },
+  (err: HttpErrorResponse)=> {
+    console.log(err.message);
+  });
+ }*/
 }
